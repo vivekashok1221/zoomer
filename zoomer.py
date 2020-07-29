14fff699b632e0260f2a73a6ec4b6a1bca3000f0
Vivek Ashok <vivekashok1221@gmail.com>
2020-07-29 11:10:14 +0200

    changes

--- zoomer.py
+++ zoomer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import argparse
 import configparser
 import zoomermodule as zm
@@ -6,9 +7,12 @@ def main(option = None):
     config = configparser.ConfigParser()
     config.read('data.ini')
     joinposn = config['VALUES']['join'].split(', ') #list(coordinates of join button)
+    joinposn = [int(x) for x in joinposn]
+    if joinposn == [0,0]:
+        print("Oops, it looks like you haven't set the positon of join button.")
+
     if option == None:
-        zoomId = zm.get_id()
-        zoomPass = zm.get_pass(zoomId)
+        zoomId,zoomPass = zm.get_id_pass(zm.get_subject())
     elif option == args.subject:
         zoomId,zoomPass = zm.get_idpass_by_subject(option)
     else:
@@ -18,12 +22,12 @@ def main(option = None):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Command line interface for zoomer.py",formatter_class = argparse.RawTextHelpFormatter)
+    parser.add_argument('-gmp','--getmousepos',action = 'store_true',help = "\ndescr:command to get mouse position \n")
     parser.add_argument('-m',dest = 'subject',metavar = 'subject',help = "\ndescr:manually enter the subject \n\n")
     parser.add_argument('-M',dest = 'ultramanual',nargs = 2,metavar = ('id','password'),help = "\ndescr:manually enter id and password \n\n")
     parser.add_argument('-a',dest = 'append',nargs = 3,metavar = ('subject', 'id', 'password'),help = "descr:append data to passwords.csv \n\n")
     parser.add_argument('-p','--passwd',dest = 'changepass',nargs = 2,metavar = ('subject','new_password'), help = 'descr:change the password corresponding to the subject \n\n') 
     parser.add_argument('-u','--updatepass',dest = 'updatepass',metavar = 'path',help = "descr:updates passwords.csv with data extracted from docx at path \n\n")
-    parser.add_argument('-gmp','--getmousepos',action = 'store_true',help = "\ndescr:command to get mouse position \n")
     args = parser.parse_args()
     
     if args.getmousepos:
--- zoomermodule.py
+++ zoomermodule.py
@@ -1,4 +1,3 @@
-from os import startfile
 import subprocess
 import csv
 import time
@@ -15,40 +14,42 @@ def get_period():
     '''Gets period based on time'''
     periods = {
         1 : (datetime.time(7,30),datetime.time(8,40)),
-        2 : (datetime.time(8,40),datetime.time(9,25)),
-        3 : (datetime.time(9,25),datetime.time(10,15)),
-        4 : (datetime.time(10,15),datetime.time(11,15)),
+        2 : (datetime.time(8,40),datetime.time(9,30)),
+        3 : (datetime.time(9,30),datetime.time(10,25)),
+        4 : (datetime.time(10,25),datetime.time(11,15)),
         5 : (datetime.time(11,15),datetime.time(12,15)),
     }
     for period in periods:
         now = datetime.datetime.now()
-        if now.strftime("%w") not in ('5','6') and (periods[period][0] <= now.time() < periods[period][1]):
+        if periods[period][0] <= now.time() < periods[period][1]:
             return  period
     print("No class at the moment")
     input("press Enter to exit...")
     raise SystemExit
 
-def get_id():
-    '''Retrieves id based on timetable'''
+def get_subject():
+    '''Retrieves subject based on timetable'''
     with open('timetable.csv','r') as timetable:
         period = get_period()
         timetableReader = list(csv.reader(timetable))
     day = int(datetime.datetime.now().strftime("%w"))+1
-    id = timetableReader[day][period]
+    subject = timetableReader[day][period]
     print("Day:",datetime.datetime.now().strftime("%A"))
     print(f"Period : {period}")
-    print(f"id : {id}")
-    return id
+    print(f"subject : {subject}")
+    return subject
 
-def get_pass(id):
-    '''Retrieves password corresponding to id'''
+def get_id_pass(subject):
+    '''Retrieves id and password corresponding to subject'''
     with open('passwords.csv','r') as passwords:
         reader = csv.DictReader(passwords)
         for row in reader:
-            if row["id"] == id:
-                print("subject:", row["subject"])
-                subprocess.run("clip",universal_newlines = True, input = row["password"])
-                return row["password"]
+            if row["subject"] == subject:
+                id = row["id"]
+                password = row["password"]
+                print("id:", id)
+                subprocess.run("clip",universal_newlines = True, input = password) #works on only windows 
+                return id, password
 
 def zoom(id,password,joinposn):
     '''Executes actions on Zoom.exe'''
@@ -63,10 +64,12 @@ def zoom(id,password,joinposn):
     time.sleep(1) #
     keyboard.type(id)
     keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
     time.sleep(3.5) #
     keyboard.type(password)
     keyboard.press(Key.enter)  
-
+    keyboard.release(Key.enter)
+    
 #utils
 
 def get_idpass_by_subject(subject):
@@ -80,7 +83,7 @@ def get_idpass_by_subject(subject):
 
 def append(values):
     with open(r'passwords.csv','a') as passwords:
-        passwords.write(f"{values[0].upper()},{values[1]},{values[2]}")
+        passwords.write(f"{values[0].upper()},{values[1]},{values[2]}\n")
     raise SystemExit
 
 def changepass(values):
@@ -104,7 +107,6 @@ def updatepass(newPassPath):
     with open(r'passwords.csv','w') as passwords:
         passwords.write('subject,id,password\n')
         doc = Document(newPassPath)
-        print(newPassPath)
         for table in doc.tables:
             for row in range(1,8):
                 subject = table.cell(row,2).text.strip()
